6945937236652e3bebc019dface1891547712d4e
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-07-03 11:12:37 +0200

    Moved all libs to vite

--- libs/editor/src/view/index.ts
+++
@@ -1,3 +0,0 @@
-export const someFunction = () => {
-  return Symbol('someFunction');
-};
--- libs/view/src/lib/nodes/index.ts
+++ libs/view/src/lib/nodes/index.ts
@@ -1,4 +1,4 @@
 export { ColumnViewNode, ColumnViewNodeOutlet } from './column-node';
 export { DividerViewNode } from './divider-node';
 export { ImageViewNode } from './image-node';
-export { ViewNode } from './view-node';
+export type { ViewNode } from './view-node';
